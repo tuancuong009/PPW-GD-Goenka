b86601adf9b0849a4f14a5e512346fc410fc02c4
QTS Coder <tester.qts@gmail.com>
2025-07-25 17:30:45 +0700

    Upload new source code

---
+++ Cutebrains/Common/Define.swift
@@ -0,0 +1,21 @@
+//
+//  Define.swift
+//  Cutebrains
+//
+//  Created by QTS Coder on 21/05/2024.
+//
+
+import UIKit
+
+struct SERVER_APP{
+    static let URL_WEB = "https://ppw-gdgpss.cutebrains.com/login/index.php"
+    static let URL_SERVER = "https://ppw-gdgpss.cutebrains.com"
+}
+
+let APP_DELEGATE = UIApplication.shared.delegate as! AppDelegate
+let API_TOKEN = "API_TOKEN"
+
+let APP_NAME = "PPW GD Goenka"
+let APP_ID = "id6749142329"
+let SECOND_CHECK_UPDATE_VERSION = 12 * 60 * 60
+let KEY_CHECK_UPDATE_VERSION = "KEY_CHECK_UPDATE_VERSION"
